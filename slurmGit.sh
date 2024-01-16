d020e3fd83d3d7e1677132f1a01c17deb514a742
bokChoy <amychoi7@snu.ac.kr>
2024-01-16 21:59:05 +0900

    update node2

---
+++ slurmGit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -i
+# 
+#SBATCH --job-name=gitPush8
+#SBATCH --output=slurmGitPush8.out
+#SBATCH --nodelist=node8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+
+export OMP_NUM_THREADS=36
+git add .
+git commit -m "update node2"
+git push
