c3caadd3f61def6964cd7d68e31e544e1ffcf0a7
bokChoy <amychoi7@snu.ac.kr>
2024-01-18 22:32:31 +0900

    add pyxrayEx

---
+++ 1_FCIDUMP/1_FCIDUMP_final/mySimul/pyxrayEx/slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -i
+# 
+#SBATCH --job-name=04_XAS
+#SBATCH --output=04_slurmXAS.out
+#SBATCH --nodelist=node5
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+conda activate pyscf
+echo $CONDA_PREFIX
+
+export OMP_NUM_THREADS=36
+python 04_FeSdi_xas.py > 04_FeSdi_xas.out
+
